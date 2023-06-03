1263cf80eb612427c4ba74c6d0cea5599e880591
Максим Костюков <enginer385@gmail.com>
2023-06-03 21:34:10 +1000

    FIxed router include

--- app/__main__.py
+++ app/__main__.py
@@ -5,7 +5,7 @@ from app.auth.router import router as auth_subrouter
 app = fastapi.FastAPI()
 
 
-app.include_router(auth_subrouter, prefix="/api/v1/auth/")
+app.include_router(auth_subrouter, prefix="/api/v1/auth")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
