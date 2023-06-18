b74554afe0cfba4d140108b0f4d359516c511f12
Максим Костюков <enginer385@gmail.com>
2023-06-18 17:08:41 +1000

    Added admin functions

--- app/good/router.py
+++ app/good/router.py
@@ -10,7 +10,7 @@ from app.auth.models import User as AuthUser
 router = APIRouter()
 
 
-@router.post("/create_good", dependencies=[Depends(AuthDeps.admin_required)])
+@router.post("/create_good", dependencies=[Depends(AuthDeps.admin_required)], tags=["goods", "admin"])
 async def create_good(
     request: Annotated[schemas.CreateGood, Depends(schemas.CreateGood)],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -18,7 +18,7 @@ async def create_good(
     pass
 
 
-@router.patch("/update_good", dependencies=[Depends(AuthDeps.admin_required)])
+@router.patch("/update_good", dependencies=[Depends(AuthDeps.admin_required)], tags=["goods", "admin"])
 async def update_good(
     request: Annotated[schemas.UpdateGood, Depends(schemas.UpdateGood)],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -26,7 +26,7 @@ async def update_good(
     pass
 
 
-@router.post("/create_brand", dependencies=[Depends(AuthDeps.admin_required)])
+@router.post("/create_brand", dependencies=[Depends(AuthDeps.admin_required)], tags=["goods", "admin"])
 async def create_brand(
     request: Annotated[schemas.CreateBrand, Depends(schemas.CreateBrand)],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -34,7 +34,7 @@ async def create_brand(
     pass
 
 
-@router.patch("/update_brand", dependencies=[Depends(AuthDeps.admin_required)])
+@router.patch("/update_brand", dependencies=[Depends(AuthDeps.admin_required)], tags=["goods", "admin"])
 async def update_brand(
     request: Annotated[schemas.UpdateBrand, Depends(schemas.UpdateBrand)],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -42,7 +42,7 @@ async def update_brand(
     pass
 
 
-@router.post("/create_category", dependencies=[Depends(AuthDeps.admin_required)])
+@router.post("/create_category", dependencies=[Depends(AuthDeps.admin_required)], tags=["goods", "admin"])
 async def create_category(
     request: Annotated[schemas.CreateBrand, Depends(schemas.CreateBrand)],
     db: Annotated[AsyncSession, Depends(get_db)],
@@ -50,7 +50,7 @@ async def create_category(
     pass
 
 
-@router.patch("/update_category", dependencies=[Depends(AuthDeps.admin_required)])
+@router.patch("/update_category", dependencies=[Depends(AuthDeps.admin_required)], tags=["goods", "admin"])
 async def update_category(
     request: Annotated[schemas.UpdateCategory, Depends(schemas.UpdateCategory)],
     db: Annotated[AsyncSession, Depends(get_db)],
