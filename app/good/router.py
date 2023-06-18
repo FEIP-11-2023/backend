6f25492da49cf04c21e9aba68abcbb966f1e5e4d
Максим Костюков <enginer385@gmail.com>
2023-06-18 16:58:51 +1000

    Added admin functions

--- app/good/router.py
+++ app/good/router.py
@@ -13,7 +13,7 @@ router = APIRouter()
 @router.post("/create_good")
 @AuthDeps.admin_required
 async def create_good(
-    request: Annotated[schemas.CreateGood, Depends()],
+    request: Annotated[schemas.CreateGood, Depends(schemas.CreateGood)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     pass
@@ -22,7 +22,7 @@ async def create_good(
 @router.patch("/update_good")
 @AuthDeps.admin_required
 async def update_good(
-    request: Annotated[schemas.UpdateGood, Depends()],
+    request: Annotated[schemas.UpdateGood, Depends(schemas.UpdateGood)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     pass
@@ -31,7 +31,7 @@ async def update_good(
 @router.post("/create_brand")
 @AuthDeps.admin_required
 async def create_brand(
-    request: Annotated[schemas.CreateBrand, Depends()],
+    request: Annotated[schemas.CreateBrand, Depends(schemas.CreateBrand)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     pass
@@ -40,7 +40,7 @@ async def create_brand(
 @router.patch("/update_brand")
 @AuthDeps.admin_required
 async def update_brand(
-    request: Annotated[schemas.UpdateBrand, Depends()],
+    request: Annotated[schemas.UpdateBrand, Depends(schemas.UpdateBrand)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     pass
@@ -49,7 +49,7 @@ async def update_brand(
 @router.post("/create_category")
 @AuthDeps.admin_required
 async def create_category(
-    request: Annotated[schemas.CreateBrand, Depends()],
+    request: Annotated[schemas.CreateBrand, Depends(schemas.CreateBrand)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     pass
@@ -58,7 +58,7 @@ async def create_category(
 @router.patch("/update_category")
 @AuthDeps.admin_required
 async def update_category(
-    request: Annotated[schemas.UpdateCategory, Depends()],
+    request: Annotated[schemas.UpdateCategory, Depends(schemas.UpdateCategory)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     pass
